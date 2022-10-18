0a3584501f0def64f54050ec6d33aa2cc3c07d5d
mario <marioworker@163.com>
2022-10-18 20:09:41 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { Button } from 'antd'
+import { ProForm, ProFormText } from '@ant-design/pro-components'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProForm
+        onFinish={async (values) => {
+          console.log(values)
+        }}
+      >
+        <ProFormText name="name" label="姓名" />
+      </ProForm>
+      <Button type="primary">Button</Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
